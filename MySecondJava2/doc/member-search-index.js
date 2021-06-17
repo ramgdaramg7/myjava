33d5cd6b3aea981e8f08453c41b83efbf8a4f227
TJ <TJ@DESKTOP-UOO4N74>
2021-06-17 21:39:13 +0900

    처음 커밋 20210617

---
+++ MySecondJava2/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"HelloJava","l":"HelloJava()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"HelloJava","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"MySecondClass","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"MySecondClass","l":"MySecondClass()","u":"%3Cinit%3E()"}];updateSearchResults();
---
+++ MySecondJava2/src/MySecondClass.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ * @author KIM
+ * 
+ *
+ */
+public class MySecondClass {
+
+	public static void main(String[] args) {
+System.out.println("Hello World2");
+System.out.println("Hello World2");
+System.out.println("Hello World2");
+System.out.println("Hello World2");
+System.out.println("Hello World2");
+System.out.println("Hello World2");
+System.out.println("Hello World2");
+System.out.println("Hello World2");
+System.out.println("Hello World2");
+System.out.println("Hello World2");
+System.out.println("Hello World2");
+System.out.println("Hello World2");
+	}
+
+}
